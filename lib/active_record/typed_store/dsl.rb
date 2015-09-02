6dec122553c1c8f7ae19fbd66623ae1ed834aa2e
Jean Boussier <jean.boussier@gmail.com>
2015-09-02 10:38:56 -0400

    Give decimals a default scale and limit (required for Rails 5.0)

--- lib/active_record/typed_store/dsl.rb
+++ lib/active_record/typed_store/dsl.rb
@@ -14,10 +14,14 @@ module ActiveRecord::TypedStore
       @columns.select(&:accessor?).map(&:name)
     end
 
-    [:string, :text, :integer, :float, :decimal, :datetime, :date, :boolean, :any].each do |type|
-      define_method(type) do |name, options={}|
-        @columns << Column.new(name, type, options.reverse_merge(accessor: @accessors))
+    [:string, :text, :integer, :float, :datetime, :date, :boolean, :any].each do |type|
+      define_method(type) do |name, options = {}|
+        @columns << Column.new(name, type, {accessor: @accessors}.merge(options))
       end
+    end
+
+    def decimal(name, options = {})
+      @columns << Column.new(name, :decimal, {accessor: @accessors, limit: 20, scale: 6}.merge(options))
     end
 
   end
