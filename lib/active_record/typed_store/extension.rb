c704ae1f7042195689886f505bb46f08ea212ae9
Sean Griffin <sean@seantheprogrammer.com>
2018-01-12 10:36:59 -0700

    Support Rails 5.2+
    As of Rails 5.2, `attribute_will_change!` no longer works for
    non-attributes. This re-implements the required methods of `Dirty`
    manually, similar to what was originally done by this gem.
    
    Note: I don't think it's appropriate to override `changes`. It's
    reasonable to expect that `model.changes` will never include keys that
    aren't also keys in `model.attributes` or `model.attribute_names`.
    I think it would be more appropriate to support the `from:` and `to:`
    options in `attribute_changed?` instead.

--- lib/active_record/typed_store/extension.rb
+++ lib/active_record/typed_store/extension.rb
@@ -8,19 +8,17 @@ module ActiveRecord::TypedStore
 
     included do
       class_attribute :typed_stores
+      self.typed_stores = {}
     end
 
     module ClassMethods
       def store_accessors
-        return [] unless typed_stores
-        typed_stores.values.map(&:accessors).flatten
+        typed_stores.each_value.flat_map(&:accessors)
       end
 
       def typed_store(store_attribute, options={}, &block)
         dsl = DSL.new(store_attribute, options, &block)
-        self.typed_stores ||= {}
-        self.typed_stores = typed_stores.deep_dup
-        self.typed_stores[store_attribute] = dsl
+        self.typed_stores = self.typed_stores.merge(store_attribute => dsl)
 
         typed_klass = TypedHash.create(dsl.fields.values)
         const_set("#{store_attribute}_hash".camelize, typed_klass)
@@ -29,11 +27,26 @@ module ActiveRecord::TypedStore
           Type.new(typed_klass, dsl.coder, subtype)
         end
         store_accessor(store_attribute, dsl.accessors)
+
+        dsl.accessors.each do |accessor_name|
+          define_method("#{accessor_name}_changed?") do
+            send("#{store_attribute}_changed?") &&
+              send(store_attribute)[accessor_name] != send("#{store_attribute}_was")[accessor_name]
+          end
+
+          define_method("#{accessor_name}_was") do
+            send("#{store_attribute}_was")[accessor_name]
+          end
+
+          define_method("restore_#{accessor_name}!") do
+            send("#{accessor_name}=", send("#{accessor_name}_was"))
+          end
+        end
       end
 
       def define_attribute_methods
         super
-        define_typed_store_attribute_methods if typed_stores
+        define_typed_store_attribute_methods
       end
 
       def undefine_attribute_methods # :nodoc:
@@ -57,6 +70,16 @@ module ActiveRecord::TypedStore
       end
     end
 
+    def changes
+      changes = super
+      self.class.store_accessors.each do |attr|
+        if send("#{attr}_changed?")
+          changes[attr] = [send("#{attr}_was"), send(attr)]
+        end
+      end
+      changes
+    end
+
     def clear_attribute_change(attr_name)
       return if self.class.store_accessors.include?(normalize_attribute(attr_name))
       super
@@ -66,16 +89,6 @@ module ActiveRecord::TypedStore
       if self.class.store_accessors.include?(normalize_attribute(attr_name))
         return public_send(attr_name)
       end
-      super
-    end
-
-    def write_store_attribute(store_attribute, key, value)
-      if typed_stores && typed_stores[store_attribute]
-        prev_value = read_store_attribute(store_attribute, key)
-        new_value = typed_stores[store_attribute].fields[key].cast(value)
-        attribute_will_change!(key.to_s) if new_value != prev_value
-      end
-
       super
     end
 
